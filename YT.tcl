07aae1fb2e37d2cf5382dea182612bc23c1abe03
hsszyman <hsszyman@gmail.com>
2019-06-03 16:50:06 -0400

    fixed terminate script

--- YT.tcl
+++ YT.tcl
@@ -1,17 +1,11 @@
 #!/usr/bin/expect --
-exp_internal 1
-debug 0
-log_user 0
 set search [exec fetch 1]
+puts $search
 
 spawn youtube-viewer $search
 expect -re "=>>.*"
-puts "before"
 set out $expect_out(buffer)
-set choice [exec fetch $out]
-puts "after"
-puts $choice
-puts $choice
+set choice [exec fetch $out $out]
 
 send -- "$choice \n"
 wait
